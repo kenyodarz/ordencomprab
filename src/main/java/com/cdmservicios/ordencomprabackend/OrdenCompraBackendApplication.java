f7f970e50a50a25c43a63f7cbf788be966dfff76
Jorge Mina <minajorge@hotmail.com>
2020-11-04 09:19:43 -0500

    Initial Commit

---
+++ src/main/java/com/cdmservicios/ordencomprabackend/OrdenCompraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cdmservicios.ordencomprabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdenCompraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdenCompraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdmservicios/ordencomprabackend/OrdenCompraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdmservicios.ordencomprabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdenCompraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
